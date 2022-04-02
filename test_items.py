9d1321c6dbb9db7fbb9d3c50b1391e91493f0339
belovanes <gogagog@mail.ru>
2022-04-02 20:38:07 +0300

    decision on Stepik_lesson_3_6_step9  # Please enter the commit message for your changes. Lines starting

--- test_items.py
+++ test_items.py
@@ -4,6 +4,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_check_add2basket_button(browser):
     browser.get(link)
-    sleep(3)
+    # sleep(3)
     button = browser.find_elements_by_css_selector("button.btn.btn-add-to-basket")
     assert button, 'Button AddToBasket not found!' # при 0 длине массива тест падает
